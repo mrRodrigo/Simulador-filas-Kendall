ba95b74d6e51957695dac81e77c785ab894318fa
Fuzzy <rodrigo.rosa.005@acad.pucrs.br>
2020-08-30 19:20:39 -0300

    Add files via upload

---
+++ Event.js
@@ -0,0 +1,11 @@
+const EnumEvent = { EXIT: 'SAIDA', ARRIVAL: 'CHEGADA'};
+
+class Event {
+  constructor(type, time, FILA){
+    this.type = type;
+    this.time = time;
+    this.FILA = FILA;
+  }
+}
+
+module.exports = { Event, EnumEvent } ;
