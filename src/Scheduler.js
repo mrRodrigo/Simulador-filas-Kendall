d18239a36b57f5a8a571b3b62964c1ae06ceea60
Joao Nascimento <joao.nascimento@sensedia.com>
2020-09-29 22:39:23 -0300

    wip: tandem

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -4,14 +4,21 @@ const { rnd, generateRandomBetween1And0 } = require('./Math');
 class Scheduler {
     constructor(queue, randomList) {
         this.queue = queue;
+        //Recebo um array de filas para processar e trabalhar.
+        //fila1 = _queues[0]. fila2 = queues[1]
+        this._queues = [];
         this.currentTime = 0;
         this.previousTime = 0;
         this.loss = 0;
+        this._loss = [];
         this.events = []; // events to be processed
         this.logEvents = []; // only for log in the end of simulation
         this.statesTime = {}; // all time for each states
         this.randomList = randomList;
     };
+
+    //O escalonador precisa ter as variáveis:
+    //
 
     setRandomList(randomList){
         this.randomList = randomList;
@@ -42,10 +49,19 @@ class Scheduler {
     processArrival(event) {
         this.countTime();
 
+        //SE FILA1 < 3
         if (this.queue.position < this.queue.capacity) {
+            //FILA1++
             this.queue.incrementPosition();
-            if (this.queue.position <= this.queue.size) this.schedulerExit();
-        } else this.loss++;
+            //SE FILA1 <= 2
+            if (this.queue.position <= this.queue.size) {
+                //ALTERAR PARA PASSAGEM DE 1->2
+                this.schedulerExit();
+            }
+        } else {
+            //this._loss[fila_atual]++;
+            this.loss++;
+        }
 
         this.schedulerArrival();
 
@@ -72,7 +88,10 @@ class Scheduler {
 
         this.queue.decrementPosition();
 
-        if (this.queue.position >= this.queue.size) this.schedulerExit();
+        //SE FILA2 >= 1
+        if (this.queue.position >= this.queue.size) {
+            this.schedulerExit();
+        }
 
         this.logEvents.push(event);
     };
@@ -90,6 +109,35 @@ class Scheduler {
             )
         );
     };
+
+    schedulerPassage() {
+
+        const { awaitMin, awaitMax } = this.queue[0];
+
+        const tempoSaida = this.currentTime + rnd(awaitMin, awaitMax, this.shiftRandom());
+
+        this.events.push(new Event('PASSAGE', tempoSaida, this.queue.position))
+
+    };
+
+    processPassage() {
+        this.countTime();
+
+        const fila1 = this._queues[0];
+        const fila2 = this._queues[1];
+
+        fila1.decrementPosition();
+
+        if (fila1.position >= fila1.size) {
+            this.schedulerPassage();
+        } if (fila2.position < fila2.capacity) {
+            fila2.incrementPosition();
+            if(fila2.position <= fila2.size) {
+                this.schedulerExit();
+            }
+        }
+
+    }
 
     getSimulationData(){
         return {
@@ -110,6 +158,8 @@ class Scheduler {
             this.processArrival(event);
         } else if (event.type === EnumEvent.EXIT) {
             this.processExit(event);
+        } else if (event.type === 'PASSAGE') {
+            this.processPassage(event);
         }
     };
 }
